bfac750a50d81ae481eb26dc762978a78a2882b6
npowell <npowell@franciene.localdomain>
2009-03-22 21:14:02 -0400

    * Fixed a bug with the url encoding that was messing up the params being passed in.
    * Added a rudimentary view

--- ritehere.rb
+++ ritehere.rb
@@ -8,22 +8,30 @@ configure do
     db.create_table :urls do
       primary_key :id
       text :url, :null => false
+      datetime :visit_time, :null => false
     end
   end
 end
 
 class Url < Sequel::Model; end
 
+get "/" do
+  redirect "/where"
+end
+
 get "/where" do
-  "Eventually this should be the listing of where I have been"
+  @urls = Url.all
+  erb :where
 end
 
 get "/here" do
   callback = "";
   url = "something went wrong"
+
   unless params[:url].nil?; url = params[:url]; end
   unless params[:callback].nil?; callback = params[:callback]; end
-  Url.create(:url => url)
 
-  return "#{callback}({'success':1})"
+  Url.create(:url => url, :visit_time => Time.now)
+
+  return "({'success':1})"
 end
